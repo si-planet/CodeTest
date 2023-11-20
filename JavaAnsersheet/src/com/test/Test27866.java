d9e4a6c09ef9ec4da232ad96fc3e741f07ca354b
Amber_Ju <sooin.ju@gmail.com>
2023-11-20 23:59:10 +0900

    Test27866
    문자열 인덱스로 추출하기 charAt(index) 사용

---
+++ JavaAnsersheet/src/com/test/Test27866.java
@@ -0,0 +1,17 @@
+package com.test;
+
+import java.io.*;
+public class Test27866
+{
+
+	public static void main(String[] args) throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String word = br.readLine();
+		int step = Integer.parseInt(br.readLine())-1; // index 는 0 부터 세기 때문에
+		
+		System.out.println(word.charAt(step));
+	}
+
+}
