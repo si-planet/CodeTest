cee1521822109fbba525bc45514107f5a7bc7c8f
Amber_Ju <sooin.ju@gmail.com>
2023-11-21 01:32:55 +0900

    Test2475
    StringTokenizer 로 나누고 해당 값을 제곱한 수를 바로 합산 -> print 할 때 %10하여 나머지
    ※ 다른 방법으론 읽어온 라인의 인덱스 값으로 해당 값들을  char 로추출 -'0' (아스키) 하여 아스키 코드 값으로 int 형 계산하여 출력

---
+++ JavaAnsersheet/src/com/test/Test2475.java
@@ -0,0 +1,24 @@
+package com.test;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+import java.util.StringTokenizer;
+public class Test2475
+{
+
+	public static void main(String[] args) throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str = br.readLine();
+		int result = 0;
+		
+		StringTokenizer st = new StringTokenizer(str, " ");
+		while(st.hasMoreTokens())
+			result += Math.pow(Integer.parseInt(st.nextToken()), 2);
+		
+		System.out.println(result%10);
+	}
+
+}
