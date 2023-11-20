2f9a54c385cc63958db0dcb0baa1e8ce3117afbe
Amber_Ju <sooin.ju@gmail.com>
2023-11-20 18:24:51 +0900

    Test2743
    String 클래스 length() 로 풀이

---
+++ JavaAnsersheet/src/com/test/Test2743.java
@@ -0,0 +1,12 @@
+package com.test;
+
+import java.io.*;
+public class Test2743
+{
+	public static void main(String[] args) throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String res = br.readLine();
+		System.out.println(res.length());
+	}
+}
