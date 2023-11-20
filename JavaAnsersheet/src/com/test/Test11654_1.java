4b4dead416d753d913536eefd3cc32b6fcfbb5ab
Amber_Ju <sooin.ju@gmail.com>
2023-11-20 18:11:56 +0900

    Test11654
    _1 : Scanner 로 String 받아와 char 형태 변환 후 ASCII 코드 저장
    _2 : System.in.read() 사용

---
+++ JavaAnsersheet/src/com/test/Test11654_1.java
@@ -0,0 +1,27 @@
+package com.test;
+
+import java.util.*;
+public class Test11654_1
+{
+
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int asc = sc.next().charAt(0);
+		// charAt() : String 에서 char 로 문자열을 잘라 문자로 반환
+		// int 타입으로 문자를 받음으로 그에 대응하는 ASCII 코드 값이 저장 되는 형태
+		
+		System.out.println(asc);
+	}
+
+}
+/*
+한 글자만 읽어와서 아스키코드 출력하는 방법
+1. Scanner 로 읽어와서 charAt() 사용
+2. System.in 을 직접 사용하여 아스키코드로 받기
+
+2번이 더 쉽고 간결하다
+
+Scanner 의 경우 정규식을 남발함으로써 성능이 떨어진다
+*/
+
---
+++ JavaAnsersheet/src/com/test/Test11654_2.java
@@ -0,0 +1,35 @@
+package com.test;
+
+import java.io.IOException;
+
+public class Test11654_2
+{
+
+	public static void main(String[] args)
+	{
+		int asc;
+		try
+		{
+			asc = System.in.read();
+			// System.in 의 read() 를 사용하여 아스키 코드 값으로 바로 받아오기
+			// 단, 예외처리 필수 -- 여기에서는 try~catch 구문을 사용하였음
+			System.out.println(asc);
+		} 
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		
+	}
+
+}
+/*
+한 글자만 읽어와서 아스키코드 출력하는 방법
+1. Scanner 로 읽어와서 charAt() 사용
+2. System.in 을 직접 사용하여 아스키코드로 받기
+
+2번이 더 쉽고 간결하다
+
+Scanner 의 경우 정규식을 남발함으로써 성능이 떨어진다
+*/
+
