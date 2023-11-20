3cbee4baa8f67d0934d3d3d5c21c68b347d46142
Amber_Ju <sooin.ju@gmail.com>
2023-11-21 01:05:25 +0900

    Test15964
    간단한 연산 -> 데이터 타입을 지정하는데 주의

---
+++ JavaAnsersheet/src/com/test/Test15964.java
@@ -0,0 +1,24 @@
+package com.test;
+
+import java.util.*;
+public class Test15964
+{
+	public static void main(String[] args)
+	{
+
+		Scanner sc = new Scanner(System.in);
+		long a, b;
+		
+		a = sc.nextLong();
+		b = sc.nextLong();
+		
+		System.out.println((a+b)*(a-b));
+	}
+}
+/*
+int 로 계산시 -> 30 점
+long 으로 계산시 -> 100 점
+
+결과 값이 int(4Byte)에 담기지 않을 수 있기 때문에
+long(8Byte)로 타입을 지정해주어야한다
+*/
