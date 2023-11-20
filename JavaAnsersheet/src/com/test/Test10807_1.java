aa6c8194715508285b81eb0a0385e6e0a8d8a394
Amber_Ju <sooin.ju@gmail.com>
2023-11-20 16:17:55 +0900

    Test10807
    _1 배열 문제 해결

---
+++ JavaAnsersheet/src/com/test/Test10807_1.java
@@ -0,0 +1,29 @@
+package com.test;
+
+import java.util.Scanner;
+
+public class Test10807_1
+{
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		int N, v, result;
+
+		N = sc.nextInt();
+		
+		int arr[] = new int[N];
+		for (int i=0; i<N; i++)
+			arr[i] = sc.nextInt();
+		
+		v = sc.nextInt();
+		result = 0;
+
+		sc.close();
+		
+		for (int i=0; i<N; i++)
+			if (arr[i]== v)
+				result++;
+		
+		System.out.println(result);
+	}
+}
