693391e3587c528c366a11f9bd41ad9ba5cb51c0
Amber_Ju <sooin.ju@gmail.com>
2023-11-21 17:10:51 +0900

    Test1152
    StringTokenizer를 공백기준으로 나눈 후 나눠진 토큰 갯수 카운트
    ※ 더 빠른 방법으로는 System.in.read 로 한 문자씩 읽어와서 ASCII 코드를 이용해 개행문자나 띄어쓰기문자에 맞닥뜨렸을때 직전 문자가 띄어쓰기 문자가 아닐경우에 +1 카운트 하는 방식이 있음

---
+++ JavaAnsersheet/src/com/test/Test1152.java
@@ -0,0 +1,16 @@
+package com.test;
+
+import java.io.*;
+import java.util.StringTokenizer;
+public class Test1152
+{
+
+	public static void main(String[] args) throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String str = br.readLine();
+		
+		StringTokenizer st = new StringTokenizer(str, " ");		
+		System.out.println(st.countTokens());
+	}
+}
