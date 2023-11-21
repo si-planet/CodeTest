3ae7b5868434f65913d5031abfabeb352c106dd5
Amber_Ju <sooin.ju@gmail.com>
2023-11-21 16:43:50 +0900

    Teat11720
    readLine()의 String 문자열 charAt()으로 ASCII 코드로 연산 (문자열 0을 빼주면 아스키코드로 동일한 숫자가 된다)

---
+++ JavaAnsersheet/src/com/test/Test11720.java
@@ -0,0 +1,21 @@
+package com.test;
+
+import java.io.*;
+
+public class Test11720
+{
+
+	public static void main(String[] args) throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int num = Integer.parseInt(br.readLine());
+
+		String str = br.readLine();
+		int result = 0;
+		for (int i=0; i<num; i++)
+			result += str.charAt(i)-'0';
+
+		System.out.println(result);
+	}
+
+}
