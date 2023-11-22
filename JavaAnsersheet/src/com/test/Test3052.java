938b87e7313b0052a366f19f5584544123f623b8
Amber_Ju <sooin.ju@gmail.com>
2023-11-22 18:24:56 +0900

    Test3052

---
+++ JavaAnsersheet/src/com/test/Test3052.java
@@ -0,0 +1,27 @@
+package com.test;
+
+import java.io.*;
+public class Test3052
+{
+
+	public static void main(String[] args) throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int[] arr = new int[42];
+		int cnt = 0;
+		
+		for (int i=0; i<10; i++)
+		{
+			int temp = Integer.parseInt(br.readLine()) % 42;
+			arr[temp]++; 
+		}
+		
+		for (int i=0; i<arr.length; i++)
+			if (arr[i]!=0)
+				cnt++;
+		
+		System.out.println(cnt);
+	}
+
+}
