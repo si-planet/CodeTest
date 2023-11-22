fa945cc163bb235124cccce97d664e3188653718
Amber_Ju <sooin.ju@gmail.com>
2023-11-22 16:40:59 +0900

    Test1157
    _1 : 첫 번째 풀이
    _2 : 간결화-재구성 풀이

---
+++ JavaAnsersheet/src/com/test/Test1157_1.java
@@ -0,0 +1,61 @@
+package com.test;
+
+import java.io.*;
+public class Test1157_1
+{
+
+	public static void main(String[] args) throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader (System.in));
+		String str = br.readLine().toUpperCase();
+		
+		int arr[] = new int[26];	// 영문자는 26 개
+        if(str.length()!=1)
+        {
+    		for (int i=0; i<str.length(); i++)
+    			arr[str.charAt(i)-65]++;
+    		
+    		int max = 0;
+    		int num=0 , cnt=1;
+    		for (int i=0; i<arr.length; i++)
+    		{
+    			if (arr[i]>max)
+    			{
+    				max = arr[i];
+    				num = i;
+    			}
+    			else if (arr[i]==max)
+    				cnt++;
+    			else
+    				continue;
+    		}
+    		
+    		if (cnt > 1)
+    			System.out.println("?");
+    		else
+    			System.out.printf("%c", (num+65));
+        }
+        
+        else if(str.length()==1)
+            System.out.printf("%c", str.charAt(0));
+	}
+
+}
+/*
+1. 입력받은 문장(단어)를 모두 대문자로 바꾸어 줌
+-> 대소문자 관계없이 카운트 + 출력 대문자
+ 
+2. arr[] 로 영문자 알파벳 갯수만큼 방을 만들어
+-> 단어 길이만큼 반복문을 돌려서 해당 (대문자-65)==(ASCII Code 숫자) 배열 방의 값을 증가 시킨다
+-> 값의 갯수 만큼 숫자가 들어있는 배열 완성
+
+3. 가장 큰 값, 그리고 그 해당 값의 방 번호, 같은 값이 존재하는지 확인
+-> 큰 값이 나올때마다 갱신
+-> 갱신될 때 같이 번호 갱신 (+65 하면 알파벳이 될 숫자)
+-> 같은 값이 되면 카운트 증가
+
+4. 마지막에 cnt 확인하여 1 이상일 경우 ? 반환 1일 경우 해당 알파벳 반환
+
+5. 1글자 일 경우 인식하지 못하는 상황이 확인되어 한 글자일 경우는 해당 알파벳 바로 출력 할 수 있게 함
+
+ */
---
+++ JavaAnsersheet/src/com/test/Test1157_2.java
@@ -0,0 +1,41 @@
+package com.test;
+
+import java.io.*;
+public class Test1157_2
+{
+
+	public static void main(String[] args) throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader (System.in));
+		String str = br.readLine().toUpperCase();
+		
+		int arr[] = new int[26];	// 영문자는 26 개
+		for (int i=0; i<str.length(); i++)
+			arr[str.charAt(i)-65]++;
+		
+		int max = -1;
+		char ch= '?';
+		
+		for (int i=0; i<arr.length; i++)
+		{
+			if (arr[i]>max)
+			{
+				max = arr[i];
+				ch = (char)(i+65);	// i 번째 방 + 65 -> 대문자 ASCII code
+			}
+			else if (arr[i]==max)
+				ch = '?';
+		}
+		System.out.println(ch);
+		
+	}
+
+}
+/*
+1번의 풀이에서 3~5 번을 재구성
+
+3. 출력 문자를 ? 로 초기화 후, max 값 갱신마다 해당 문자를 +65 한 대문자로 갱신
+   또한, max 값 갱신 중에 똑같은 값을 발견하면 다시 출력문자를 ? 로 갱신
+   전체 배열을 전부 확인해야하는 이유는
+   -> 현재 저장된 max 값 이상으로 들어가있는 값이 있는지 전체 확인해야하기 때문
+*/
