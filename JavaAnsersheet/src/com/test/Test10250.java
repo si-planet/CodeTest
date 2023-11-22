a6e6b79057baed7df6673cd8c00b0b8905068ba4
Amber_Ju <sooin.ju@gmail.com>
2023-11-22 19:12:53 +0900

    Test10250
    예외가 되는 값에 주의 하기

---
+++ JavaAnsersheet/src/com/test/Test10250.java
@@ -0,0 +1,39 @@
+package com.test;
+
+import java.util.*;
+public class Test10250
+{
+
+	public static void main(String[] args)
+	{
+		Scanner sc = new Scanner(System.in);
+		
+		int t = sc.nextInt();
+		for (int i=0; i<t; i++)
+		{
+			int h, w, n; 
+			h = sc.nextInt();	// 층 6
+			w = sc.nextInt();	// 방 10
+			n = sc.nextInt();	// 손님 60 -> 
+			
+			int x,y;
+			// y 층 구하기
+			// x 호 구하기
+			if (n%h==0)
+			{
+				y = h*100;
+				x = n / h;
+			}
+			else
+			{
+				y = (n%h)*100;
+				x = ( n / h ) + 1;
+			}
+			
+			System.out.println(x+y);
+			
+			
+		}
+	}
+
+}
