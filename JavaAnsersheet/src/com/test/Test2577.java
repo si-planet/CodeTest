a32443072a2f52e4e19165bae55ce6fce8e81fb0
Amber_Ju <sooin.ju@gmail.com>
2023-11-22 17:11:35 +0900

    Test2577
    int -> String -> int
    많이 변환하는 것 같은데 print 문도 StringBuilder 로 한번에 출력해서 그런가 80ms 밖에 안걸림

---
+++ JavaAnsersheet/src/com/test/Test2577.java
@@ -0,0 +1,30 @@
+package com.test;
+
+import java.io.*;
+public class Test2577
+{
+
+	public static void main(String[] args) throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int a, b, c;
+		int[] arr = new int[10]; // 0~9
+		
+		a = Integer.parseInt(br.readLine());
+		b = Integer.parseInt(br.readLine());
+		c = Integer.parseInt(br.readLine());
+		
+		String str = String.valueOf(a*b*c);
+		StringBuilder sb = new StringBuilder();
+		
+		for (int i=0; i<str.length(); i++)
+			arr[str.charAt(i)-'0']++;
+		
+		for (int i : arr)
+			sb.append(i).append("\n");
+		
+		System.out.println(sb);
+	}
+
+}
