92327c8342210491eaade5ffaecfd460bd743e91
Amber_Ju <sooin.ju@gmail.com>
2023-11-22 23:34:21 +0900

    Test10809
    문자' ' , 문자열 " " 혼동 조심

---
+++ JavaAnsersheet/src/com/test/Test10809.java
@@ -0,0 +1,31 @@
+package com.test;
+
+import java.io.*;
+public class Test10809
+{
+
+	public static void main(String[] args) throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String word = br.readLine();
+		
+		int[] arr = new int[26];
+		
+		for (int i=0; i< arr.length; i++)
+			arr[i] = -1;
+		
+		for (int i=0; i<word.length(); i++)
+		{
+			if (arr[word.charAt(i)-'a'] != -1)
+				continue;
+
+			arr[word.charAt(i)-'a'] = i;
+		}
+		
+		for (int i=0; i < arr.length; i++)
+			System.out.print(arr[i]+" ");
+			
+	}
+
+}
