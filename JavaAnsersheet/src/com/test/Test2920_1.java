920157b7a50de24784985c6140cc79f703ee9882
Amber_Ju <sooin.ju@gmail.com>
2023-11-22 18:05:55 +0900

    Test2920
    결과값을 mixed 로 두고
    1 과 8로 시작하는 결과 값을 asc, desc 로 둔 후, 다음 숫자와 비교하여 +1 또는 -1이 되지 않을 경우 mixed 로 바꾸어 반복문을 빠져나간다

---
+++ JavaAnsersheet/src/com/test/Test2920_1.java
@@ -0,0 +1,57 @@
+package com.test;
+
+import java.io.*;
+import java.util.StringTokenizer;
+public class Test2920_1
+{
+
+	public static void main(String[] args) throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String result = "mixed";
+		
+		String str = br.readLine();
+		StringTokenizer st = new StringTokenizer(str, " ");
+		
+		int first = Integer.parseInt(st.nextToken());  
+		if (first == 1)
+		{
+			result = "ascending";
+			while (st.hasMoreElements())
+			{
+				first++;
+				if (first != Integer.parseInt(st.nextToken()))
+				{
+					result = "mixed";
+					break;
+				}
+				
+			}
+		}
+		else if (first == 8)
+		{
+			result = "descending";
+			while (st.hasMoreElements())
+			{
+				first--;
+				if (first != Integer.parseInt(st.nextToken()))
+				{
+					result = "mixed";
+					break;
+				}
+			}
+		}
+		
+		System.out.println(result);
+			
+	}
+
+}
+/*
+첫 번째 풀이 -> 오답
+
+- ascending 이나 descending 에서 한 번이라도 다음 번호가 맞으면
+  result 값이 변해 버리기 때문에 break 만 써서는 안되고
+  다시 mixed 로 재정의 해주어야 함
+*/
