603c5e0fe267ab5bec853b3d7a37791b8d03ffd9
Amber_Ju <sooin.ju@gmail.com>
2023-11-23 16:37:23 +0900

    Test1978
    소수 1 예외처리, 카운트 증가 처리 위치 주의

---
+++ JavaAnsersheet/src/com/test/Test1978.java
@@ -0,0 +1,45 @@
+package com.test;
+
+import java.io.*;
+import java.util.StringTokenizer;
+public class Test1978
+{
+
+	public static void main(String[] args) throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader (System.in));
+		int N = Integer.parseInt(br.readLine());
+		
+		boolean isPrime;
+		int cnt = 0;
+		
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		// 입력받은 갯수 만큼 확인
+		for (int i=0; i<N; i++)
+		{
+			isPrime = true;
+			
+			int temp = Integer.parseInt(st.nextToken());
+			// 1은 소수가 아님 -> 예외처리
+			if (temp==1)
+				continue;
+
+			// 2부터 자기자신 직전까지 나눠지면 소수가 아님 
+			for (int j=2; j<temp; j++)
+			{
+				if (temp%j==0)
+				{
+					isPrime = false;
+					break;
+				}
+			}
+			
+			if (isPrime)
+				cnt+=1;
+		}
+
+		System.out.println(cnt);
+		
+	}
+
+}
