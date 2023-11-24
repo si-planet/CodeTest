7802cd9880b1ed34c24c3ac36acb7f576e2b84e0
Amber_Ju <sooin.ju@gmail.com>
2023-11-24 18:10:35 +0900

    Test1259
    펠린드롬수 - 거꾸로 읽어도 동일한 수

---
+++ JavaAnsersheet/src/com/test/Test1259.java
@@ -0,0 +1,38 @@
+package com.test;
+
+import java.io.*;
+public class Test1259
+{
+
+	public static void main(String[] args) throws IOException
+	{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String str;
+		while (!(str = br.readLine()).equals("0"))
+		{
+			int arr[] = new int[str.length()];
+			for (int i=0; i<arr.length; i++)
+				arr[i] = str.charAt(i);
+			
+			System.out.println(search(arr));
+		}
+	}
+	
+	static String search(int[] arr)
+	{
+		String result = "yes";
+		
+		for (int i=0; i<(arr.length/2); i++)
+		{
+			for (int j=(arr.length-i-1); j<(arr.length-i); j++)
+			{
+				if (arr[i]!=arr[j])
+					return result = "no";
+			}
+		}
+		
+		return result;
+	}
+
+}
