d65fa44f38bb595eac5676927b4e50a977eeca63
si-planet <137014394+si-planet@users.noreply.github.com>
2023-12-14 12:01:19 +0900

    Test11047
    Test11047
    
    최소 동전 개수 찾기

---
+++ BaekjoonJava/src/com/answersheet/Test11047.java
@@ -0,0 +1,43 @@
+package com.answersheet;
+
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Tesr11047 
+{
+    public static void main(String args[]) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader (System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine()," ");
+        
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+        
+        int[] coin = new int[N]; 
+        int count = 0;
+        
+        for(int i=0; i<coin.length; i++)
+            coin[i] = Integer.parseInt(br.readLine());
+        
+        int i = N-1;
+        
+        while(K>0)
+        {
+            
+            if(coin[i]<=K)
+            {
+                count += (K/coin[i]);
+                K -= coin[i]*(K/coin[i]);
+            }
+            
+            if(K==0)
+                break;
+                
+            i--;
+        
+        }
+        
+        System.out.println(count);
+        
+    }
+}
