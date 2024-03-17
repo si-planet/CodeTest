951d9dc5790f3654c268305acd8f153e2650619c
si-planet <sooin.ju@gmail.com>
2024-03-17 15:29:31 +0900

    UPDATE // Test2579.java
    DP 문제 풀이 수정
    단계를 풀기 위해서 일정한 규칙을 찾을 필요가 있음 - 다시 풀어볼 것

--- BaekjoonJava/src/com/answersheet/Test2579.java
+++ BaekjoonJava/src/com/answersheet/Test2579.java
@@ -6,65 +6,44 @@ import java.io.InputStreamReader;
 
 public class Test2579
 {
-	static int[] stair;	// 다이나믹 프로그래밍 다시 확인
+	static int[] scores;
 	
 	public static void main(String[] args) throws IOException
 	{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
+		// 계단 개수
 		int N = Integer.parseInt(br.readLine());
-		stair = new int[N+1];	// 0 은 시작점 , 1 ~ N 까지 계단 점수로 활용
-		
+		// 계단 점수 데이터
+		int[] stair = new int[N+1];	// 0 은 시작점 , 1 ~ N 까지 계단 점수로 활용
 		for (int i=1; i<stair.length; i++)
 			stair[i] = Integer.parseInt(br.readLine());
 		br.close();
 
-		// 게임
-		int max = getScore(N+1, 0, 0);
-		System.out.println(max);
+		// 최대 점수 배열
+		scores = new int[N+1];
 		
-		
-	}// end main()
+		if(N==1) {
+			scores[1] = stair[1];	// 첫 칸에 올라가는 최대 점수
+		} else if(N==2) {
+			scores[2] = stair[2]+stair[1];	// 둘째 칸에 올라가는 최대 점수
+		} else {
+			scores[1] = stair[1];
+			scores[2] = stair[2]+stair[1];
+			// 셋째 칸에 올라가는 최대 점수 = -1칸 + 자기 값 또는 -2칸 + 자기 값
+			scores[3] = Math.max(stair[1]+stair[3], stair[2]+stair[3]);	
 
-	// 시작하는 지점을 마지막 계단으로 설정(필수)
-	// 1칸 or 2칸 이동 : end-1 , end-2
-	// 1칸을 연속으로 간 횟수가 2가 되면 무조건 2 칸 움직임 + count 초기화
-	private static int getScore(int end, int sum, int count)
-	{
-		if (end < 1)
-			return sum;
-		else
-		{
-			// 계단 점수 더하기
-			sum += stair[end];
-			
-			// count 에 따라 1 또는 2칸 내려가기
-			// 주의 : end 의 값은 0 또는 자연수여야함
-			if (count==2)
-			{
-				if (end > 2)
-				{
-					return getScore(end-2, sum, 0);
-				}
-				else
-				{
-					return -1;
-				}
-			}
-			else
-			{
-				if (end > 2)
-				{
-					return Math.max(getScore(end-1, sum, count+1), getScore(end-2, sum, 0));
-				}
-				else 
-				{
-					return getScore(end-1, sum, count+1);
-				}
-					
+			for(int i=4; i<scores.length; i++) {
+				// 1. 두칸 + 한칸 올라온 경우 = 두칸 시작점까지 최대 값 + 두번째 칸 값 + 도착한 값 
+				// 2. 두칸 올라온 경우 = 두 칸전 까지의 최대 값 + 도착한 값
+				scores[i] = Math.max( (scores[i-3] + stair[i-1] + stair[i]) , (scores[i-2] + stair[i]) );
+				//System.out.println(i + " : " + scores[i]);
 			}
 		}
 		
-	}// end getScore()
+		// 출력
+		System.out.println(scores[N]);
+		
+	}// end main()
 	
 }
